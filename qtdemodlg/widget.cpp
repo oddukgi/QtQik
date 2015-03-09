9931fc011ceb41d35717093488c713304dff4b0c
darongyi <cutysunmi@gmail.com>
2015-03-09 16:35:12 +0900

    add 6buttons

--- qtdemodlg/widget.cpp
+++ qtdemodlg/widget.cpp
@@ -6,9 +6,48 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    connect(ui->pushBtn_home,SIGNAL(clicked()),this,SLOT(on_pushBtn_home_clicked()));
+    connect(ui->pushBtn_Graph1,SIGNAL(clicked()),this,SLOT(on_pushBtn_Graph1_clicked()));
+    connect(ui->pushBtn_Graph2,SIGNAL(clicked()),this,SLOT(on_pushBtn_Graph2_clicked()));
+    connect(ui->pushBtn_Schedule,SIGNAL(clicked()),this,SLOT(on_pushBtn_Schedule_clicked()));
+    connect(ui->pushBtn_Value,SIGNAL(clicked()),this,SLOT(on_pushBtn_Value_clicked()));
+    connect(ui->pushBtn_Raw,SIGNAL(clicked()),this,SLOT(on_pushBtn_Raw_clicked()));
+
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+
+void Widget::on_pushBtn_home_clicked()
+{
+
+}
+
+void Widget::on_pushBtn_Graph1_clicked()
+{
+
+}
+
+void Widget::on_pushBtn_Graph2_clicked()
+{
+
+}
+
+void Widget::on_pushBtn_Schedule_clicked()
+{
+
+}
+
+void Widget::on_pushBtn_Value_clicked()
+{
+
+}
+
+void Widget::on_pushBtn_Raw_clicked()
+{
+
 }
--- qtdemodlg/widget.h
+++ qtdemodlg/widget.h
@@ -2,7 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include <QImage>
 namespace Ui {
 class Widget;
 }
@@ -14,6 +14,19 @@ class Widget : public QWidget
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
+
+private slots:
+    void on_pushBtn_home_clicked();
+
+    void on_pushBtn_Graph1_clicked();
+
+    void on_pushBtn_Graph2_clicked();
+
+    void on_pushBtn_Schedule_clicked();
+
+    void on_pushBtn_Value_clicked();
+
+    void on_pushBtn_Raw_clicked();
 
 private:
     Ui::Widget *ui;
